2754975808e5c4b145a14ddfc50c7ebd399132d0
GoGei <rich.290401@gmail.com>
2025-08-11 22:22:18 +0200

    MP-1 Setup project: - Add flake8

--- fabfile.py
+++ fabfile.py
@@ -2,26 +2,31 @@ import os
 from fabric import task
 from django.conf import settings as dj_settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
 
 
 @task
 def runserver(c):
     host = dj_settings.SITE_HOST
     port = dj_settings.SITE_PORT
-    c.run(f"./manage.py runserver {host}:{port}", pty=True)
+    c.run(f'./manage.py runserver {host}:{port}', pty=True)
 
 
 @task
 def compilerequirements(c):
-    c.run("uv pip compile requirements.txt -o requirements.lock")
+    c.run('uv pip compile requirements.txt -o requirements.lock')
 
 
 @task
 def makemessages(c):
-    c.run(f"./manage.py makemessages --all --no-location")
+    c.run(f'./manage.py makemessages --all --no-location')
 
 
 @task
 def compilemessages(c):
-    c.run(f"./manage.py compilemessages")
+    c.run(f'./manage.py compilemessages')
+
+@task
+def check(c):
+    c.run('./manage.py check')
+    c.run('flake8')
