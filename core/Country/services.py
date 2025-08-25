e09cea9c4ef9929b2864ac86442759807a20c598
GoGei <rich.290401@gmail.com>
2025-08-25 09:40:58 +0200

    MP-7 Countries Handbooks: - Add country services

---
+++ core/Country/services.py
@@ -0,0 +1,65 @@
+import json
+import requests
+from requests.exceptions import Timeout, HTTPError
+
+from django.db.models import QuerySet
+
+from .const import DEFAULT_FIXTURE, EXTERNAL_API_DOCUMENTATION_URL, EXTERNAL_API_URL
+from .models import Country
+
+
+class LoadCountriesException(Exception):
+    pass
+
+
+def import_countries_from_fixture(archive_not_mentioned: bool = True, fixture: str = DEFAULT_FIXTURE):
+    mentioned: set[int] = set()
+    data = json.load(open(fixture, 'r'))
+
+    for item in data:
+        obj, _ = Country.objects.update_or_create(
+            ccn3=item['ccn3'],
+            defaults={
+                'cca2': item['cca2'],
+                'name': item['name']['common'],
+            }
+        )
+        mentioned.add(obj.id)
+        if not obj.is_active:
+            obj.restore()
+
+    if archive_not_mentioned:
+        Country.objects.exclude(id__in=mentioned).archive()
+
+
+def import_countries_from_external_api(fields: tuple) -> QuerySet[Country]:
+    if not fields:
+        raise LoadCountriesException(f'Please, specify fields to get. Documentation: {EXTERNAL_API_DOCUMENTATION_URL}')
+
+    try:
+        params = {'fields': fields}
+        response = requests.get(url=EXTERNAL_API_URL, params=params)
+        response.raise_for_status()
+    except Timeout as e:
+        raise LoadCountriesException(f'Timeout: {str(e)}')
+    except HTTPError as e:
+        raise LoadCountriesException(f'HTTPError status is not succeeded: {str(e)}')
+
+    try:
+        data = response.json()
+    except Exception as e:
+        raise LoadCountriesException(f'Unable to load data from countries API: {str(e)}')
+
+    mentioned: set[int] = set()
+    for item in data:
+        obj, _ = Country.objects.update_or_create(
+            ccn3=item['ccn3'],
+            defaults={
+                'cca2': item['cca2'],
+                'name': item['name']['common'],
+            }
+        )
+        mentioned.add(obj.id)
+        if not obj.is_active:
+            obj.restore()
+    return Country.objects.filter(id__in=mentioned)
--- core/Country/tests/test_country.py
+++ core/Country/tests/test_country.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
+from unittest.mock import patch, MagicMock
+
+from requests import HTTPError, Timeout
+
 from ..models import Country
 from ..factories import CountryFactory
+from ..services import import_countries_from_fixture, import_countries_from_external_api, LoadCountriesException
 
 
 class CountryTestCase(TestCase):
@@ -13,3 +18,67 @@ class CountryTestCase(TestCase):
         obj_id = obj.id
         obj.delete()
         self.assertNotIn(obj_id, Country.objects.all().values_list('id', flat=True))
+
+    def test_import_countries_from_fixture(self):
+        c1 = Country.objects.count()
+        import_countries_from_fixture()
+        c2 = Country.objects.count()
+        self.assertTrue(c2 > c1)
+
+
+class ImportCountriesFromExternalApiTestCase(TestCase):
+    @patch('core.Country.services.requests.get')
+    def test_import_countries_from_external_api_success(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = [
+            {
+                'name': {'common': 'Test', 'official': 'Test'},
+                'cca2': 'TT',
+                'ccn3': '000',
+            }
+        ]
+        mock_response.raise_for_status.return_value = None
+        mock_get.return_value = mock_response
+
+        c1 = Country.objects.count()
+        qs = import_countries_from_external_api(fields=('name', 'cca2', 'ccn3'))
+        c2 = Country.objects.count()
+
+        self.assertTrue(c2 > c1)
+        self.assertEqual(qs.count(), 1)
+        obj = qs.first()
+        self.assertEqual(obj.name, 'Test')
+        self.assertEqual(obj.cca2, 'TT')
+        self.assertEqual(obj.ccn3, '000')
+
+    @patch('core.Country.services.requests.get')
+    def test_import_countries_from_external_api_no_fields(self, mock_get):
+        with self.assertRaises(LoadCountriesException):
+            import_countries_from_external_api(fields=())
+
+    @patch('core.Country.services.requests.get')
+    def test_import_countries_from_external_api_timeout(self, mock_get):
+        mock_get.side_effect = Timeout('Test timeout')
+        with self.assertRaises(LoadCountriesException) as e:
+            import_countries_from_external_api(fields=('test',))
+        self.assertIn('Timeout', str(e.exception))
+        mock_get.assert_called()
+
+    @patch('core.Country.services.requests.get')
+    def test_import_countries_from_external_api_http_error(self, mock_get):
+        mock_get.side_effect = HTTPError('Test')
+        with self.assertRaises(LoadCountriesException) as e:
+            import_countries_from_external_api(fields=('test',))
+        self.assertIn('HTTPError', str(e.exception))
+        mock_get.assert_called()
+
+    @patch('core.Country.services.requests.get')
+    def test_import_countries_from_external_api_invalid_json(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.raise_for_status.return_value = None
+        mock_response.json.side_effect = ValueError('Not a JSON')
+        mock_get.return_value = mock_response
+        with self.assertRaises(LoadCountriesException) as ctx:
+            import_countries_from_external_api(fields=('name', 'cca2', 'ccn3'))
+        self.assertIn('Not a JSON', str(ctx.exception))
+        mock_get.assert_called()
