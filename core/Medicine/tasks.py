71086b86fa88867ff4a891f4ca0f60131cbd76d2
GoGei <rich.290401@gmail.com>
2025-08-30 12:32:48 +0200

    MP-8 Medicine handbooks: - Add celery task to load allergy types

--- core/Medicine/tasks.py
+++ core/Medicine/tasks.py
@@ -10,3 +10,12 @@ def extract_allergy_cause():
         return True
     except Exception:
         return False
+
+
+@app.task(ignore_result=True)
+def extract_allergy_types():
+    try:
+        call_command('extract_allergy_types')
+        return True
+    except Exception:
+        return False
