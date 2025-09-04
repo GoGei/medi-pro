ee25173d859abda5d5ac9c7ab078ae3646c7f3bb
GoGei <rich.290401@gmail.com>
2025-09-04 08:40:44 +0200

    MPO-43 Fix all imports: - Fix colors import: add fixture/data import choice, add validation of import

--- core/Colors/services.py
+++ core/Colors/services.py
@@ -1,15 +1,50 @@
 import json
+
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+from django.db.transaction import atomic
+
 from .const import DEFAULT_FIXTURE
 from .models import EmployeeColors
 
+regex = r'#[0-9a-fA-F]{6}'
+color_regex = RegexValidator(regex=regex)
 
-def import_colors_from_fixture(archive_not_mentioned: bool = True, fixture: str = DEFAULT_FIXTURE):
+
+class LocaColorsFromFixtureException(Exception):
+    pass
+
+
+def run_validator(item: dict, raise_exc: bool = True):
+    item['sideline'] = item['sideline'].upper()
+    item['background'] = item['background'].upper()
+
+    try:
+        color_regex(item['sideline'])
+    except ValidationError:
+        msg = _('Sideline {sideline} does not match regex: {regex}').format(regex=regex, sideline=item['sideline'])
+        raise LocaColorsFromFixtureException(msg)
+
+    try:
+        color_regex(item['background'])
+    except ValidationError:
+        msg = _('Background {background} does not match regex: {regex}').format(regex=regex,
+                                                                                background=item['background'])
+        raise LocaColorsFromFixtureException(msg)
+
+
+@atomic
+def import_colors_from_fixture(archive_not_mentioned: bool = True, fixture: str = DEFAULT_FIXTURE, data: list[dict] = None):
     mentioned: set[int] = set()
-    data = json.load(open(fixture, 'r'))
+    if not data:
+        data = json.load(open(fixture, 'r'))
 
     for item in data:
+        run_validator(item=item, raise_exc=True)
+
         base_field = item.pop('sideline')
-        obj, _ = EmployeeColors.objects.update_or_create(
+        obj, created = EmployeeColors.objects.update_or_create(
             sideline=base_field,
             defaults=item
         )
@@ -19,3 +54,5 @@ def import_colors_from_fixture(archive_not_mentioned: bool = True, fixture: str 
 
     if archive_not_mentioned:
         EmployeeColors.objects.exclude(id__in=mentioned).archive()
+    if EmployeeColors.objects.active().filter(is_default=True).count() > 1:
+        raise LocaColorsFromFixtureException(_('After import there are more than 1 default color'))
