5c36395b4561bf1f3f4ffaf6f196d5574d727786
GoGei <rich.290401@gmail.com>
2025-09-08 08:54:00 +0200

    MPO-41 Update admin clinic pre-settings: - Update requirements and settings

--- hosts.py
+++ hosts.py
@@ -3,6 +3,7 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host('', 'Public.urls', name='public'),
+    host('api', 'Api.urls', name='api'),
     host('clinic', 'Clinic.urls', name='clinic'),
     host('admin', 'Admin.urls', name='admin'),
 )
