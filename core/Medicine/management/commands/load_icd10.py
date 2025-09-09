41bba4fadec32290cc2f96e889df9d6cd54646f0
GoGei <rich.290401@gmail.com>
2025-09-09 23:42:11 +0200

    MPO-26 Add handbook update log: - PEP8 fix

--- core/Medicine/management/commands/load_icd10.py
+++ core/Medicine/management/commands/load_icd10.py
@@ -14,6 +14,7 @@ class Command(BaseCommand):
     """
     help = 'Load ICD-10 codes from HL7 FHIR'
 
+    @atomic
     def handle(self, *args, **options):
         archive = zipfile.ZipFile('core/Medicine/fixture/icd-10.zip', 'r')
         table_data = archive.read('section111validicd10-jan2025_0.xlsx')
@@ -24,33 +25,32 @@ class Command(BaseCommand):
         headers = next(rows)
         created_count = updated_count = total = archived = 0
 
-        with atomic():
-            for row in rows:
-                item = dict(zip(headers, row))
-                total += 1
+        for row in rows:
+            item = dict(zip(headers, row))
+            total += 1
 
-                try:
-                    icd10, created = ICD10.objects.update_or_create(
-                        code=item['CODE'],
-                        defaults={
-                            'name': item['SHORT DESCRIPTION (VALID ICD-10 FY2025)'],
-                            'source': MedicineHandbookSources.CMS_GOV
-                        }
-                    )
+            try:
+                icd10, created = ICD10.objects.update_or_create(
+                    code=item['CODE'],
+                    defaults={
+                        'name': item['SHORT DESCRIPTION (VALID ICD-10 FY2025)'],
+                        'source': MedicineHandbookSources.CMS_GOV
+                    }
+                )
 
-                    if created:
-                        created_count += 1
-                    else:
-                        updated_count += 1
+                if created:
+                    created_count += 1
+                else:
+                    updated_count += 1
 
-                    if item['NF EXCL'] == 'Y':
-                        icd10.archive()
-                        archived += 1
+                if item['NF EXCL'] == 'Y':
+                    icd10.archive()
+                    archived += 1
 
-                    if total % 500 == 0:
-                        self.stdout.write(f'Processed {total} ICD-10 codes')
-                except Exception as e:
-                    raise CommandError(f'For item {item} raised error: {e}')
+                if total % 500 == 0:
+                    self.stdout.write(f'Processed {total} ICD-10 codes')
+            except Exception as e:
+                raise CommandError(f'For item {item} raised error: {e}')
 
         self.stdout.write(self.style.SUCCESS(f'Successfully created {created_count} ICD-10 codes'))
         self.stdout.write(self.style.SUCCESS(f'Successfully updated {updated_count} ICD-10 codes'))
